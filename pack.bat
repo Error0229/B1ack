7d34a5b91a52341ed21cdff6e48ba853147f13e0
catJam <loginerrore04@gmail.com>
2024-03-28 04:34:50 +0800

    Add pack.bat script to create zip files for Chrome and Firefox extensions

---
+++ pack.bat
@@ -0,0 +1,10 @@
+
+: Navigate to the chrome directory, zip the contents, then move back
+cd chrome
+zip -r ../chrome/extension.zip .
+cd ..
+
+: Navigate to the firefox directory, zip the contents, then move back
+cd firefox
+zip -r ../firefox/addon.zip .
+cd ..
