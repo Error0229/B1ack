37f7c64e9c1bcf7215d8e27c127132a5f569c491
catJam <loginerrore04@gmail.com>
2024-03-29 14:56:16 +0800

    Add support for downloading PDF files and update browser action icons

--- chrome/background.js
+++ chrome/background.js
@@ -3,16 +3,30 @@ let urlToDownload = null;
 let refererURL = null;
 let filename = null;
 DOMAIN = "https://istudy.ntut.edu.tw/*";
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (!tab.url.startsWith("https://istudy.ntut.edu.tw/")) {
+    SetIcon("waiting");
+  }
+});
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function (tab) {
+    if (!tab.url.startsWith("https://istudy.ntut.edu.tw/")) {
+      SetIcon("waiting");
+    }
+  });
+});
 // Listening for a request to capture the referer URL
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
     if (details.url.startsWith("https://istudy.ntut.edu.tw/learn/path/viewPDF.php")) {
       refererURL = details.url;
+      SetIcon("waiting");
     }
     if (details.url.startsWith("https://istudy.ntut.edu.tw/learn/path/getPDF.php")) {
       urlToDownload = details.url;
       // file name start with id= ... end with .pdf
       filename = details.url.split("id=")[1].split(".pdf")[0];
+      SetIcon("downloadable");
     }
   },
   { urls: [DOMAIN] });
@@ -56,3 +70,24 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     });
   }
 });
+
+function SetIcon(state) {
+  if (state == "waiting") {
+    idleIcons = {
+      "16": "images/zzz_16.png",
+      "32": "images/zzz_32.png",
+      "64": "images/zzz_64.png",
+      "128": "images/zzz_128.png"
+    }
+    chrome.browserAction.setIcon({ path: idleIcons });
+  }
+  else if (state == "downloadable") {
+    okIcons = {
+      "16": "images/ok_16.png",
+      "32": "images/ok_32.png",
+      "64": "images/ok_64.png",
+      "128": "images/ok_128.png"
+    }
+    chrome.browserAction.setIcon({ path: okIcons });
+  }
+}
